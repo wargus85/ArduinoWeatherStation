44d28c0b8a8f41c8c8cca5fb46037826bd60f892
wargus <warren@warbel.net>
2018-06-10 21:28:41 +0800

    added time function

--- sketches/output_json/output_json.ino
+++ sketches/output_json/output_json.ino
@@ -6,14 +6,43 @@ Source code for the http server from: https://www.arduino.cc/en/Tutorial/WebServ
 
 #include <SPI.h>
 #include <Ethernet.h>
-
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-IPAddress ip(10, 60, 204, 199);
-EthernetServer server(80);
-
+#include <EthernetUdp.h>
 
 char databuffer[35];
 double temp;
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+unsigned long epoch;
+//IPAddress ip(10, 60, 204, 199);
+EthernetServer server(80);
+
+unsigned int localPort = 8888;       // local port to listen for UDP packets
+char timeServer[] = "time.nist.gov"; // time.nist.gov NTP server
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+// A UDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+void sendNTPpacket(char* address) {
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
 
 void getBuffer()                                                                    //Get weather status data
 {
@@ -97,11 +126,25 @@ float BarPressure()                                                             
 
 void setup(){
   // start the Ethernet connection and the server:
-  Ethernet.begin(mac, ip);
+  Ethernet.begin(mac) == 0;
   server.begin();
+  Udp.begin(localPort);
 }
 void loop(){
 
+sendNTPpacket(timeServer);
+delay(1000);
+if (Udp.parsePacket()) {
+  // We've received a packet, read the data from it
+  Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+  unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+  unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+  unsigned long secsSince1900 = highWord << 16 | lowWord;
+  const unsigned long seventyYears = 2208988800UL;
+  epoch = secsSince1900 - seventyYears;
+    delay(10000);
+  Ethernet.maintain();
+}
 //pull the data from the serial port
 //getBuffer();
 // listen for incoming clients
@@ -120,7 +163,7 @@ if (client) {
             client.println("HTTP/1.1 200 OK");
             client.println("Content-Type: text/html");
             client.println("Connection: close");  // the connection will be closed after completion of the response
-            client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+            client.println("Refresh: 20");  // refresh the page automatically every 5 sec
             client.println();
             client.println("<!DOCTYPE HTML>");
             client.println("<html>");
@@ -144,7 +187,9 @@ if (client) {
             client.print(RainfallOneHour());
             client.print(",\"24h\":");
             client.print(RainfallOneDay());
-            client.print("}");
+            client.print("},");
+            client.println("\"time\":");
+            client.print(epoch);
             client.println("}");
             client.println("</html>");
             break;
