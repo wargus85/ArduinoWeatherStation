e0215ed314e876a2af7ecd7e481caa5ef83d77ec
Warren Argus <warren.argus@curtin.edu.au>
2018-07-03 16:56:24 +0800

    updated

--- met_office_wow/upload_data.py
+++ met_office_wow/upload_data.py
@@ -9,6 +9,8 @@ import requests
 
 # Define some variables
 
+testData = {'coordlocal': {'lat': -32.0, 'lon': 115.86}, 'weather': {'temp': 17.78, 'pressure': 1010.7, 'humidity': 51}, 'localrain': {'24h': 10, '1h': 0}, 'localdt': 1530601625, 'wind': {'cardinal': 'SW', 'localspeed': 1.79, 'localdeg': 225, 'localgust': 4.02}}
+
 #get the date
 date = time.strftime("%Y-%m-%d",time.gmtime())
 hours = time.strftime("%H",time.gmtime())
@@ -19,5 +21,6 @@ localdt = date+"+"+hours+"%3A"+minutes+"%3A"+seconds
 
 #get the current data from the arduino
 
-request = requests.get(url='10.60.204.181')
-print(request.json())
+#request = requests.get(url='http://10.60.204.181')
+#print(request.json())
+print(testData)
