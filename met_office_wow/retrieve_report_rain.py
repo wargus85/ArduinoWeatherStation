290972be895a85b57f872db440e70f7adec9c417
wargus85 <warren@warbel.net>
2018-07-04 21:31:31 +0800

    started working on reporting rain

---
+++ met_office_wow/retrieve_report_rain.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests
+import time
+import json
+
+# date format in the json we pass to the query must be in the format of:
+# 2018-07-04T09:00:00
+# 24 hours ago is 86400 seconds
+#get the epoc in seconds
+#yesterday = time.time()-86400
+#I need to get the rainfall at 
+today = time.time()
+date = time.strftime("%Y-%m-%dT09:00:00",time.gmtime(today)) #will return the time at 9am today
+
+
+#GET weather/_search 
+query = {
+    "query": {
+        "range" : {
+            "@timestamp" : {
+                "gte" : "2018-07-04T09:00:00",
+                "lt" : "2018-07-04T09:00:10"
+            }
+        }
+    }
+}
